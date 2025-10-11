eec49d962948db413af913e75cd94472012120e9
Joakim Wiksten <c22jwn@cs.umu.se>
2025-10-11 16:22:54 +0200

    Revert "Added RafioInfo"
    This reverts commit 854291cd1e40cb31e6dc441bc444b6d06f540bf9.

--- Java/Ou3appjava/RadioInfo/src/Parser.java
+++
@@ -1,8 +0,0 @@
-/**
- * Interface for parsers for parsing Radio information
- * @author Joakim Wiksten (c22jwn)
- * @version 2023-01-03
- */
-public interface Parser<T> {
-    T run() throws Exception;
-}
